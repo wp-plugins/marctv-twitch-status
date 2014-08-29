60762f0bc4b3126dfd3c63c28dcd71fae6c96e15
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-29 13:20:26 +0000

    Switched from justin.tv api to twitch api.
    git-svn-id: https://plugins.svn.wordpress.org/marctv-twitch-status/trunk@975231 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery.marctv-twitch-status.js
+++ jquery.marctv-twitch-status.js
@@ -1,9 +1,9 @@
 (function($) {
   $(document).ready(function($) {
-    $.getJSON('http://api.justin.tv/api/stream/summary.json?channel=' + marctvtwitchsettings.channelname + '&jsonp=?', function(a) {
-      if (a.streams_count > 0) {
-        $(marctvtwitchsettings.selector).append('<li class="twitch-status menu-item"><a title="' + a.viewers_count + ' viewers" href="' + marctvtwitchsettings.url + '">Live!</a></li>');
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + marctvtwitchsettings.channelname + '?callback=?', function(a) {
+      if (a.stream) {
+        $(marctvtwitchsettings.selector).append('<li class="twitch-status menu-item"><a target="_blank" title="' + a.stream.viewers + ' viewers" href="' + marctvtwitchsettings.url + '">Live!</a></li>');
       }
     });
   });
-})(jQuery); 
+})(jQuery);
