d9859aafe99635da2d372d91d22a9cd8600bc983
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-04 11:38:56 +0000

    * Menu item is no longer appended to submenu items by default. This may break compatibility for some people.  * Plugin rewritten in OO PHP5.  * German translation updated.
    git-svn-id: https://plugins.svn.wordpress.org/marctv-twitch-status/trunk@1038082 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pages/settings.php
@@ -0,0 +1,66 @@
+<?php
+if (!defined('ABSPATH')) {
+    die(__('Cheatin&#8217; uh?'));
+}
+
+if (get_option($this->pluginPrefix .  '-menuselector') == '') {
+    update_option($this->pluginPrefix .  '-menuselector',$this->menuselector);
+}
+
+?>
+
+<div id="wrap">
+    <h1><?php _e('Twitch Status Settings', 'marctv-twitch-status'); ?></h1>
+
+    <form method="post" action="options.php">
+
+        <?php settings_fields($this->pluginPrefix . '-settings-group'); ?>
+        <?php do_settings_sections($this->pluginPrefix . '-settings-group'); ?>
+
+
+        <table class="form-table">
+            <tbody>
+            <tr valign="top">
+                <th scope="row"><label
+                        for="<?php echo $this->pluginPrefix; ?>-channelname"><?php _e('Twitch Channel', 'marctv-twitch-status'); ?></label></th>
+                <td><input name="<?php echo $this->pluginPrefix; ?>-channelname" type="text" id="<?php echo $this->pluginPrefix; ?>-channelname" value="<?php echo get_option($this->pluginPrefix .  '-channelname',$this->channelname) ?>" class="regular-text">
+
+                    <p class="description"><?php _e('The Twitch.TV channelname of the navigation menu item.', 'marctv-twitch-status'); ?></p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row"><label for="<?php echo $this->pluginPrefix; ?>-channelurl">Link URL</label></th>
+                <td><input name="<?php echo $this->pluginPrefix; ?>-channelurl" type="url" id="<?php echo $this->pluginPrefix; ?>-channelurl" value="<?php echo get_option($this->pluginPrefix .  '-channelurl', $this->channelurl); ?>" class="regular-text">
+
+                    <p class="description"><?php _e('The URL of the navigation menu item. This is where the users will end up after they clicked on the link.', 'marctv-twitch-status'); ?></p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row"><label for="<?php echo $this->pluginPrefix; ?>-menuselector">jQuery selector</label></th>
+                <td><input name="<?php echo $this->pluginPrefix; ?>-menuselector" type="text" id="<?php echo $this->pluginPrefix; ?>-menuselector" value="<?php echo get_option($this->pluginPrefix .  '-menuselector', $this->menuselector); ?>" class="large-text">
+
+                    <p class="description"><?php _e('The jQuery selector of the primary menu. Leave blank for default settings.', 'marctv-twitch-status'); ?></p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row"><label for="mtw_status">Status</label></th>
+                <td>
+                    <ul id="mtvchannelstatus">
+                        <li class="offline"><?php printf(__('Twitch Channel %s is currently offline.','marctv-twitch-status'),
+                            '<strong>' . get_option($this->pluginPrefix .  '-channelname') . '</strong>')?>
+                        </li>
+                    </ul>
+                    <p class="description"><?php _e('Here appears the appended menu item if your twitch channel is actively broadcasting.', 'marctv-twitch-status'); ?></p>
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+
+        <?php submit_button(); ?>
+
+    </form>
+</div>
